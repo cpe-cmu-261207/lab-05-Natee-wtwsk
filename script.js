7a9c2e0aa35c12fab2e940b26022e94da2464505
Natee <gg977120@gmail.com>
2022-08-04 22:51:17 +0700

    done.

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if(inputAdd.value == ''){
+    alert("Todo cannot be empty");
+    return;
+  }
+  else{
+    addTodo(inputAdd.value, false);
+    inputAdd.value = '';
+  }
+  saveTodo();
 };
 
 function addTodo(title, completed) {
@@ -29,20 +38,61 @@ function addTodo(title, completed) {
   deleteBtn.className = "btn btn-danger";
 
   //your code here
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
   //append todo to HTML...
+  todoCtn.insertBefore(div, todoCtn.children[0]);
   //define buttons event...
+  div.onmouseover = () => {
+    div.children[1].style.display = "";
+    div.children[2].style.display = "";
+  };
+  div.onmouseout = () => {
+    div.children[1].style.display = "none";
+    div.children[2].style.display = "none";
+  };
+  doneBtn.onclick = () => {
+    if(div.children[0].style.textDecoration != "line-through"){
+      div.children[0].style.textDecoration = "line-through";
+      saveTodo();
+    }
+    else{
+      div.children[0].style.textDecoration = "";
+      saveTodo();
+    }
+  }
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  }
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const arr = {title: todoDiv.children[0].innerText, completed: false};
+    if(todoDiv.children[0].style.textDecoration == "line-through") arr.completed = true;
+    data.push(arr);
   }
   //your code here
+  localStorage.clear();
+  data.reverse();
+  const dataStr = JSON.stringify(data);
+  localStorage.setItem("todoListData", dataStr);
 }
 
 function loadTodo() {
   //your code here
+  const dataStr = localStorage.getItem("todoListData");
+  const data = JSON.parse(dataStr);
+
+  for(const todoObj of data){
+    addTodo(todoObj.title, todoObj.completed);
+  }
 }
 
 loadTodo();
